c3d4d87cca34025845494a2e2cd83cdc7f6d2606
Anup Sathya <anupsathya@Anups-MacBook-Pro.local>
2022-09-26 02:36:52 -0500

    Polling

--- server/routeHandlers.go
+++ server/routeHandlers.go
@@ -40,12 +40,10 @@ func wshandler(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		t, msg, err := conn.ReadMessage()
-		fmt.Println(t)
 		if err != nil {
 			break
 		}
 		conn.WriteMessage(t, msg)
-		fmt.Println(msg)
 	}
 }
 
--- websocket-app/src/App.tsx
+++ websocket-app/src/App.tsx
@@ -1,45 +1,23 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import './App.css';
-
-const socket = new WebSocket("ws://139.144.18.143:8080/vs");
-
-function App() {
-  const [message, setMessage] = useState('')
-  const [inputValue, setInputValue] = useState('')
-
-  useEffect(() => {
-    socket.onopen = () => {
-      setMessage('Connected')
-    };
-
-    socket.onmessage = (e) => {
-      setMessage("Get message from server: " + e.data)
-    };
-
-    return () => {
-      socket.close()
+import React from 'react';
+ 
+<ReactPolling
+  url={'url to poll'}
+  interval= {3000} // in milliseconds(ms)
+  retryCount={3} // this is optional
+  onSuccess={() => console.log('handle success')}
+  onFailure={() => console.log('handle failure')} // this is optional
+  method={'GET'}
+  headers={headers object} // this is optional
+  body={JSON.stringify(data)} // data to send in a post call. Should be stringified always
+  render={({ startPolling, stopPolling, isPolling }) => {
+    if(isPolling) {
+      return (
+        <div> Hello I am polling</div>
+      );
+    } else {
+      return (
+        <div> Hello I stopped polling</div>
+      );
     }
-  }, [])
-
-  const handleClick = useCallback((e) => {
-    e.preventDefault()
-
-    socket.send(JSON.stringify({
-      message: inputValue
-    }))
-  }, [inputValue])
-
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value)
-  }, [])
-
-  return (
-    <div className="App">
-      <input id="input" type="text" value={inputValue} onChange={handleChange} />
-      <button onClick={handleClick}>Send</button>
-      <pre>{message}</pre>
-    </div>
-  );
-}
-
-export default App;
+  }}
+/>
